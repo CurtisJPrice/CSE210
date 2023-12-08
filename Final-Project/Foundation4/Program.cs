03dcb30b672cac99969bc5af18d5944e7553b886
Your Name <you@example.com>
2023-12-07 16:33:17 -0800

    finale progress

---
+++ Final-Project/Foundation4/Program.cs
@@ -0,0 +1,17 @@
+class Program
+{
+    static void Main()
+    {
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0));
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 4.8));
+        activities.Add(new Cycling(new DateTime(2022, 11, 3), 30, 20.0));
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 30, 20));
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
